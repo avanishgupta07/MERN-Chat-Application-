4d53c64cd260372282192797e05ed5b546e8ff54
avanishgupta07 <avanishguptaaza@gmail.com>
2024-03-21 23:34:45 +0530

    ADD singup Page

--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -1,8 +1,28 @@
-import React from 'react'
 
-export const Signup = () => {
+import { FormLabel, FormControl } from '@chakra-ui/form-control';
+import React, { useState } from 'react';
+import { Input } from '@chakra-ui/input';
+import { VStack } from '@chakra-ui/layout';
+
+export const Signup= () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, Setpassword] = useState();
+  const [confirmpassword, setConfirmpassword] = useState();
+  const [pic, setPic] = useState();
+
   return (
-    <div>Signup</div>
-  )
-}
- 
+    <VStack spacing="5px" color="black">
+      <FormControl id="first-name" isRequired>
+        <FormLabel>Name'</FormLabel>
+        <Input
+          placeholder="Enter Your Name"
+          onChange={(e) => setName(e.target.value)}
+
+        />
+
+      </FormControl>
+    </VStack>
+  );
+};
+
